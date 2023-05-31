36cb3b2a6bdd15228b91d03f14f2bc0c26a94d14
Amir Ekramian <amir.ekramian@samansalamat.ir>
2023-05-31 13:46:05 +0330

    Add Redirect Path

--- src/router/index.js
+++ src/router/index.js
@@ -17,12 +17,12 @@ const routes = [
     component: signupview
   },
   {
-    path: '/Books',
+    path: '/books',
     name: 'BookShow',
     component: BookShow
   },
   {
-    path: '/Books/:id',
+    path: '/books/:id',
     name: 'BookDetail',
     component: BookDetail,
     props:true
@@ -34,6 +34,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path:'/all-books',
+    redirect:'/books'
   }
 ]
 
